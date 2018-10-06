7797e84e2c2f4aa7cb251273ed469a3a27b0ebd9
Kevin McShea <kevinmcshea@gmail.com>
2018-10-05 17:08:34 -0700

    Early array exercises and hash chapter

---
+++ Arrays/exercise1.rb
@@ -0,0 +1,7 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 4
+if arr.include?(number) == true
+	puts "The number #{number} is located in the array"
+else
+	puts "The number #{number} is not located in the array"
+end
---
+++ Arrays/exercise2.rb
@@ -0,0 +1,10 @@
+arr = ["b", "a"]
+arr = arr.product(Array(1..3))
+puts arr
+arr.first.delete(arr.first.last)
+puts arr
+
+	
+arr = ["b", "a"]
+arr = arr.product([Array(1..3)])
+arr.first.delete(arr.first.last)
